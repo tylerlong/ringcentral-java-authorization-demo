14c8d00cea69f9683b3107803944f438d305d4a4
Tyler Long <tyler4long@gmail.com>
2019-06-24 16:30:40 -0700

    Show the login link

--- src/main/java/com/ringcentral/MyWebSite.java
+++ src/main/java/com/ringcentral/MyWebSite.java
@@ -1,38 +1,54 @@
 package com.ringcentral;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.ringcentral.definitions.TokenInfo;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
-public class MyWebSite extends AbstractHandler
-{
-    @Override
-    public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) 
-        throws IOException, ServletException
-    {
-        response.setStatus(HttpServletResponse.SC_OK);
-        // response.setHeader("Validation-Token", request.getHeader("Validation-Token"));
-        // if(request.getMethod() == "POST")
-        // {
-        //     String body = request.getReader().lines().collect(java.util.stream.Collectors.joining(System.lineSeparator()));
-        //     System.out.println(body);
-        //     // do whatever with body
-        // }
-        response.getWriter().println("OK");
-        baseRequest.setHandled(true);
-    }
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
-    public static void main(String[] args) throws Exception
-    {
+public class MyWebSite extends AbstractHandler {
+    public static void main(String[] args) throws Exception {
         Server server = new Server(8080);
+        MyWebSite myWebSite = new MyWebSite();
         server.setHandler(new MyWebSite());
         server.start();
         server.join();
+    }
+
+    private static String TOKEN_KEY = "rc-token";
+    private static String REDIRECT_URI = "http://localhost:8080/oauth2callback";
+
+    @Override
+    public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        response.setContentType("text/html; charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        Cookie[] cookiesArray = request.getCookies();
+        List<Cookie> cookies = Arrays.asList(cookiesArray);
+        List<Cookie> filteredCookies = cookies.stream().filter(c -> c.getName() == TOKEN_KEY).collect(Collectors.toList());
+        RestClient rc = new RestClient(
+                System.getenv("RINGCENTRAL_CLIENT_ID"),
+                System.getenv("RINGCENTRAL_CLIENT_SECRET"),
+                System.getenv("RINGCENTRAL_SERVER_URL")
+        );
+        if (filteredCookies.size() > 0) {
+            String tokenString = filteredCookies.get(0).getValue();
+            rc.token = com.alibaba.fastjson.JSON.parseObject(tokenString, TokenInfo.class);
+        } else if (request.getRequestURI() != "/oauth2callback") {
+            response.getWriter().println("<h2>RingCentral Authorization Code Flow Authentication</h2><a href=\"" + rc.authorizeUri(REDIRECT_URI) + "\">Login RingCentral Account</a>");
+        }
+
+//        System.out.println(request.getRequestURI());
+//        response.getWriter().println("OK");
+        baseRequest.setHandled(true);
     }
 }
